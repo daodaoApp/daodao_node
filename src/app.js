f4555097fd65614b54525f870bee15b088056143
holynova <holy_nova@126.com>
2018-11-08 16:45:49 +0800

    添加bodyParser

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ const Koa = require('koa');
 const config = require('./config.js');
 const app = new Koa()
 const log = require('./debugTools')
+const bodyParser = require('koa-bodyparser');
+
+
+app.use(bodyParser())
+
+app.use()
 
 app.listen(config.port)
+
 log(`app is running on port ${config.port}`)
